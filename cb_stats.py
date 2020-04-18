a2169e7ab8f237969fd8ef85c1caa2b2e655a80b
Shawn Stolsig <shawnstolsig@gmail.com>
2020-04-17 18:43:03 -0700

    need to fix date

--- cb_stats.py
+++ cb_stats.py
@@ -4,77 +4,91 @@ from datetime import datetime, timedelta
 
 # update session ids for each new season
 SESSION_IDS = {
-    '22/1': '1.1',
-    '23/1': '1.2',
-    '25/1': '1.3',
-    '26/1': '1.4',
-    '29/1': '2.1',
-    '30/1': '2.2',
-    '1/2': '2.3',
-    '2/2': '2.4',
-    '5/2': '3.1',
-    '6/2': '3.2',
-    '8/2': '3.3',
-    '9/2': '3.4',
-    '12/2': '4.1',
-    '13/2': '4.2',
-    '15/2': '4.3',
-    '16/2': '4.4',
-    '19/2': '5.1',
-    '20/2': '5.2',
-    '22/2': '5.3',
-    '23/2': '5.4',
-    '26/2': '6.1',
-    '27/2': '6.2',
-    '29/2': '6.3',
-    '1/3': '6.4',
-    '4/3': '7.1',
-    '5/3': '7.2',
-    '7/3': '7.3',
-    '8/3': '7.4',
+    '15/4': '1.1',
+    '16/4': '1.2',
+    '18/4': '1.3',
+    '19/4': '1.4',
+    '22/4': '2.1',
+    '23/4': '2.2',
+    '25/4': '2.3',
+    '26/4': '2.4',
+    '29/4': '3.1',
+    '30/4': '3.2',
+    '2/5': '3.3',
+    '3/5': '3.4',
+    '6/5': '4.1',
+    '7/5': '4.2',
+    '9/5': '4.3',
+    '10/5': '4.4',
+    '13/5': '5.1',
+    '14/5': '5.2',
+    '16/5': '5.3',
+    '17/5': '5.4',
+    '20/5': '6.1',
+    '21/5': '6.2',
+    '23/5': '6.3',
+    '24/5': '6.4',
+    '27/5': '7.1',
+    '28/5': '7.2',
+    '30/5': '7.3',
+    '31/5': '7.4',
+    '3/6': '8.1',
+    '4/6': '8.2',
+    '6/6': '8.3',
+    '7/6': '8.4',
 }
 
 # update both SHIP_INDEX_TABLE and csv_string headers whenever new ships added
 SHIP_INDEX_TABLE = {
     'Yamato': 0,
-    'Montana': 1,
-    'Ohio': 2,
-    'Kremlin': 3,
-    'Großer Kurfürst': 4, 
-    'Conqueror': 5,
-    'Thunderer': 6,
-    'République': 7, 
-    'Bourgogne': 8,
-    'Zaō': 9,
-    'Des Moines': 10,
-    'Salem': 11,
-    'Hindenburg': 12,
-    'Goliath': 13,
-    'Henri IV': 14,
-    'Venezia': 15,
-    'Yoshino': 16,
-    'Puerto Rico': 17,
-    'Moskva': 18,
-    'Stalingrad': 19,
-    'Worcester': 20,
-    'Smolensk': 21,
-    'Minotaur': 22,
-    'Colbert': 23,
-    'Shimakaze': 24,
-    'Harugumo': 25,
-    'Hayate': 26,
-    'Gearing': 27,
-    'Somers': 28,
-    'Grozovoi': 29,
-    'Khabarovsk': 30,
-    'Z-52': 31,
-    'Daring': 32,
-    'Kléber': 33,    
-    'Marceau': 34,
-    'Paolo Emilio': 35,
-    'Yueyang': 36
+    'Shikishima': 1,
+    'Montana': 2,
+    'Ohio': 3,
+    'Kremlin': 4,
+    'Großer Kurfürst': 5, 
+    'Conqueror': 6,
+    'Thunderer': 7,
+    'République': 8, 
+    'Bourgogne': 9,
+    'Zaō': 10,
+    'Des Moines': 11,
+    'Salem': 12,
+    'Hindenburg': 13,
+    'Goliath': 14,
+    'Henri IV': 15,
+    'Venezia': 16,
+    'Yoshino': 17,
+    'Puerto Rico': 18,
+    'Petropavlovsk': 19,
+    'Moskva': 20,
+    'Stalingrad': 21,
+    'Worcester': 22,
+    'Alexander Nevsky': 23,
+    'Smolensk': 24,
+    'Minotaur': 25,
+    'Colbert': 26,
+    'Shimakaze': 27,
+    'Harugumo': 28,
+    'Hayate': 29,
+    'Gearing': 30,
+    'Somers': 31,
+    'Grozovoi': 32,
+    'Khabarovsk': 33,
+    'Z-52': 34,
+    'Daring': 35,
+    'Kléber': 36,    
+    'Marceau': 37,
+    'Paolo Emilio': 38,
+    'Halland': 39,
+    'Småland': 40,
+    'Yueyang': 41,
+    'Hakuryū': 42,
+    'Midway': 43,
+    'F.D. Roosevelt': 44,
+    'Manfred von Richthofen': 45,
+    'Audacious': 46,
 }
-CSV_STRING = "Date,Session,Clan,Rating,Opponent,#,Map,Notes,Ya,Mo,Oh,Kr,GK,Co,Th,Re,Bo,Za,DM,Sa,Hi,Go,He,Ve,Yo,PR,Mk,St,Wo,Sm,Mi,Co,Sh,Hg,Hy,Ge,So,Gr,Kh,52,Da,Kl,Ma,PE,YY,X,,0,Points\n"
+CSV_STRING = "Date,Session,Clan,Rating,Opponent,#,Map,Notes,Ya,Sk,Mo,Oh,Kr,GK,Co,Th,Re,Bo,Za,DM,Sa,Hi,Go,He,Ve,Yo,PR,Pp,Mk,St,Wo,Nv,Sm,Mi,Co,Sh,Hg,Hy,Ge,So,Gr,Kh,52,Da,Kl,Ma,PE,Hd,Sd,YY,Hk,Md,FR,MR,Au,X,,0,Points\n"
 
 # get a list of all json files in working directory
 def get_json_files():
@@ -140,6 +154,14 @@ def translate_battle_data(battle, csv_string):
     date_time_obj_utc = datetime.fromisoformat(date_time_iso)
     date_time_obj_local = date_time_obj_utc - timedelta(days=1)
     date_time = f'{date_time_obj_local.day}/{date_time_obj_local.month}'
+    
+    data_time_obj_new_local = datetime.fromtimestamp(float(date_time_iso)
+
+    print(f'date_time_iso: {date_time_iso}')
+    print(f'data_time_obj_new_local: {data_time_obj_new_local}')
+    print(f'date_time_obj_utc: {date_time_obj_utc}')
+    print(f'date_time_obj_local: {date_time_obj_local}')
+    print(f'date_time: {date_time}')
 
     # convert 0 point battles to "S" for struggle
     if rating_delta == 0:
@@ -178,7 +200,7 @@ def translate_battle_data(battle, csv_string):
             sl[x] = ''
 
     # add battle as line in csv string
-    csv_string += f"{date_time},{SESSION_IDS[date_time]},{own_clan_tag},{own_clan_rating},{opponent_tag},manbear67's script,{map_name},,{sl[0]},{sl[1]},{sl[2]},{sl[3]},{sl[4]},{sl[5]},{sl[6]},{sl[7]},{sl[8]},{sl[9]},{sl[10]},{sl[11]},{sl[12]},{sl[13]},{sl[14]},{sl[15]},{sl[16]},{sl[17]},{sl[18]},{sl[19]},{sl[20]},{sl[21]},{sl[22]},{sl[23]},{sl[24]},{sl[25]},{sl[26]},{sl[27]},{sl[28]},{sl[29]},{sl[30]},{sl[31]},{sl[32]},{sl[33]},{sl[34]},{sl[35]},{sl[36]},,,{result},{rating_delta}\n"
+    csv_string += f"{date_time},{SESSION_IDS[date_time]},{own_clan_tag},{own_clan_rating},{opponent_tag},script,{map_name},,{sl[0]},{sl[1]},{sl[2]},{sl[3]},{sl[4]},{sl[5]},{sl[6]},{sl[7]},{sl[8]},{sl[9]},{sl[10]},{sl[11]},{sl[12]},{sl[13]},{sl[14]},{sl[15]},{sl[16]},{sl[17]},{sl[18]},{sl[19]},{sl[20]},{sl[21]},{sl[22]},{sl[23]},{sl[24]},{sl[25]},{sl[26]},{sl[27]},{sl[28]},{sl[29]},{sl[30]},{sl[31]},{sl[32]},{sl[33]},{sl[34]},{sl[35]},{sl[36]},,,{result},{rating_delta}\n"
 
     return csv_string
 
